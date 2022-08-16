b6690077cb8d798c51b05b8640b2fc47a4340beb
ANOVNI <78624983+anovni@users.noreply.github.com>
2022-08-16 14:54:24 +0200

    Update darkai.sh

--- darkai.sh
+++ darkai.sh
@@ -7,7 +7,7 @@
 #NOI SIAMO ANONYMOUS!
 
 
-#01/08/18
+#15/08/22
 
 clear
 # Variables
@@ -26,7 +26,7 @@ blue='\e[1;34m'
 cyan='\e[1;36m'
 red='\e[1;31m'
 
-figlet TUAN B4DUT  | lolcat
+figlet DARK'AI  | lolcat
 
 echo -b "_____________________________________________________________" | lolcat
 echo -b "TYPE      : TOOLS INSTALLER $green " |lolcat
