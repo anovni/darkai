e381dd641bafe97b78939c3944d36d96c8e10587
ANOVNI <78624983+anovni@users.noreply.github.com>
2022-08-16 17:22:07 +0200

    Update darkai.sh

--- darkai.sh
+++ darkai.sh
@@ -32,7 +32,7 @@ echo  " Installazione Hacking Tools        _______" | lolcat
 echo  " VERSIONE : v.1 IT                 |.-----.|" |lolcat
 echo  " TOTALE   : 312 TOOLS              ||x . x||" | lolcat 
 echo  " AUTORE   : TuanB4Dut              ||_.-._||" | lolcat
-echo  " EDITING  : @ANOVI1                `--)-(--`"
+echo  " EDITING  : @ANOVI1                `--)-(--`" | lolcat
 echo  "╭─OpAnonIta                       __[=== o]___" |lolcat
 echo  "╰─https://opanonita.wordpress.com| ANONYMOUS:|\ $green " |lolcat
 echo  " NEL CODICE NOI CREDIAMO         `-=========-`() $green " |lolcat
